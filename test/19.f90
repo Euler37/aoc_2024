3367e608684f23f11ee1b094852855848d3a86fb
Euler37 <xianzhangfuren@outlook.com>
2024-12-19 16:32:43 +0800

    Update 19.f90

--- test/19.f90
+++ test/19.f90
@@ -1,6 +1,7 @@
 program main
    use aoc_2024
    use string_mod
+   use set_mod
    use iso_fortran_env
    implicit none
    character(len=3000)::str
@@ -9,10 +10,15 @@ program main
    integer::res,ios,start,end,i
    integer(8)::res2,num
    integer::maxlen
+   type(set)::st
    open(10,file="data/19.txt")
    read(10,"(A)")str
    pattern=split(str,", ",.true.)
    maxlen=maxval(len(pattern))
+   call st%init(eq,bit)
+   do i=1,size(pattern)
+      call st%append(pattern(i))
+   end do
    read(10,*) ! blank line
    res=0
    res2=0
@@ -36,7 +42,8 @@ contains
       integer::i
       if(flag)return
       do i=start,end
-         if(str(start:i) .in. pattern)then
+         ! if(str(start:i) .in. pattern)then
+          if(string(str(start:i)) .in. st)then
             if(i==end)then
                flag=.true.
                return
@@ -52,12 +59,52 @@ contains
       integer::i,j
       a=0
       do i=1,end
-         if(str(1:i) .in. pattern) a(i)=1
+         ! if(str(1:i) .in. pattern) a(i)=1
+         if(string(str(1:i)) .in. st)a(i)=1
          do j=0,maxlen-1
             if(i-j<=1)cycle
-            if(str(i-j:i) .in. pattern) a(i)=a(i)+a(i-j-1)
+            ! if(str(i-j:i) .in. pattern) a(i)=a(i)+a(i-j-1)
+            if(string(str(i-j:i)) .in. st) a(i)=a(i)+a(i-j-1)
          end do
       end do
       res=a(end)
    end function dp
+
+   subroutine eq(a,b)
+      class(*),intent(inout)::a
+      class(*),intent(in)::b
+      select type(a); type is (integer)
+      select type(b); type is (integer)
+      a=b
+      end select; end select 
+
+      select type(a); type is (real)
+      select type(b); type is (real)
+      a=b
+      end select; end select 
+
+      select type(a); type is (character(len=*))
+      select type(b); type is (character(len=*))
+      a=b
+      end select; end select 
+      
+
+      select type(a); class is (string)
+      select type(b); class is (string)
+      a%str=b%str
+      end select; end select 
+   end subroutine eq
+
+   subroutine bit(a,b)
+      use iso_c_binding
+      class(*),intent(in),target::a
+      integer(1),intent(inout),pointer::b(:)
+      select type(a)
+      type is (integer)          ; call c_f_pointer(c_loc(a),b,[4])
+      type is (real)             ; call c_f_pointer(c_loc(a),b,[4])
+      type is (character(len=*)) ; call c_f_pointer(c_loc(a),b,[len(a)])
+      class is (string)          ; call c_f_pointer(c_loc(a%str),b,[len(a%str)])
+      end select
+   end subroutine bit
+
 end program main
