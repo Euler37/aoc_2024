97a45dca258733d7afbac8d31e2f98499abb46d4
Euler37 <xianzhangfuren@outlook.com>
2024-12-19 22:31:45 +0800

    Update string_mod.f90

--- src/string_mod.f90
+++ src/string_mod.f90
@@ -30,7 +30,6 @@ contains
       res=len(str%str)
    end function len_string
 
-
    logical function instrarray(str,arr)result(res)
       character(len=*),intent(in)::str
       type(string),intent(in)::arr(:)
@@ -47,7 +46,6 @@ contains
    logical function instringarray(str,arr)result(res)
       type(string),intent(in)::str
       type(string),intent(in)::arr(:)
-      integer::i
       res=instrarray(str%str,arr)
    end function instringarray
 
@@ -62,25 +60,19 @@ contains
       this%str=str
    end subroutine string_construct
 
-   function split_string(str,sep,skip)result(res)
+   subroutine split_string(str,sep,res,skip)
       type(string),intent(in)::str
       character(len=*),intent(in)::sep
       type(string),allocatable::res(:)
       logical,intent(in)::skip
-      if (sep==" ")then
-         res=split(adjustl(str%str),sep,skip)
-      else
-         res=split(str%str,sep,skip)
-      end if
-   end function split_string
+      call split(str%str,sep,res,skip)
+   end subroutine split_string
 
-   function split_str(str,sep,skip)result(res)
+   subroutine split_str(str,sep,res,skip)
       character(len=*),intent(in)::str
       character(len=*),intent(in)::sep
       logical,intent(in)::skip
-      type(string),allocatable::res(:)
-      type(string),allocatable::cap(:)
-      type(string)::tmp
+      type(string),allocatable,intent(out)::res(:)
       integer::start,end,l,ls,i,num
       start=1
       end=1
@@ -91,24 +83,15 @@ contains
          start=end
          if(skip)then
             do i=end,ls,l
-               if(str(i:i+l-1)==sep)then
-                  start=start+l
-               else
-                  exit
-               end if
+               if(str(i:i+l-1)/=sep)exit
+               start=start+l
             end do
          end if
-         end=index(str(start:),sep)
-         if(end==0)exit
-         end=end+start-1
-         tmp=str(start:end-1)
+         end=index(str(start:),sep)+start
+         if(end==start)exit
          num=num+1
-         end=end+l
       end do
-      if(ls>=start)then
-         tmp=str(start:ls)
-         num=num+1
-      end if
+      if(ls>=start)num=num+1
       allocate(res(num))
       num=0
       start=1
@@ -117,37 +100,32 @@ contains
          start=end
          if(skip)then
             do i=end,ls,l
-               if(str(i:i+l-1)==sep)then
-                  start=start+l
-               else
-                  exit
-               end if
+               if(str(i:i+l-1)/=sep)exit
+                start=start+l
             end do
          end if
          end=index(str(start:),sep)
          if(end==0)exit
          end=end+start-1
-         tmp=str(start:end-1)
          num=num+1
-         res(num)=tmp
+         res(num)=str(start:end-1)
          end=end+l
       end do
       if(ls>=start)then
-         tmp=str(start:ls)
          num=num+1
-         res(num)=tmp
+         res(num)=str(start:ls)
       end if
-   end function split_str
+   end subroutine split_str
 
    impure elemental subroutine print_string(this)
       class(string),intent(in)::this
       write(*,*,delim="quote")this%str
    end subroutine print_string
 
-   subroutine final_string(this)
+   elemental subroutine final_string(this)
       type(string),intent(inout)::this
       if(allocated(this%str))deallocate(this%str)
-   end subroutine
+   end subroutine final_string
 
    function string_view(s,n)result(res)
       character(len=1),intent(in),target::s(*)
@@ -162,5 +140,4 @@ contains
       character(len=1),pointer::res(:,:)
       res(1:n,1:m)=>s(1:n*m)
    end function string_view2d
-
 end module string_mod
