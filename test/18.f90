0df5178a2294ccfcd6a2ed661df3e82fca0c43c4
Euler37 <xianzhangfuren@outlook.com>
2024-12-20 16:02:57 +0800

    Update 18.f90

--- test/18.f90
+++ test/18.f90
@@ -5,12 +5,10 @@ program main
    integer::a(-1:n+1,-1:n+1)
    integer::x,y,i
    integer,parameter::move(2,4)=reshape([-1,0,1,0,0,-1,0,1],[2,4])
-   integer,parameter::rotate(2,4)=reshape([3,4,3,4,1,2,1,2],[2,4])
-   integer::flag(4,-1:n+1,-1:n+1),minlen,ios,j
+   integer::flag(-1:n+1,-1:n+1),minlen,ios,j
    type node
       integer::lens
       integer::idx(2)
-      integer::mv
    end type node
    a=0
    a(-1,-1:n+1)=1
@@ -46,30 +44,27 @@ contains
       integer,intent(in)::idx(2)
       type(heap)::m
       type(node)::x
-      integer::id,idy(2)
-      call m%init(n*n*4,node(0,[0,0],0),eq,cmp,sw)
-      call m%insert(node(0,idx,1))
-      call m%insert(node(0,idx,2))
-      call m%insert(node(0,idx,3))
-      call m%insert(node(0,idx,4))
+      integer::i,idy(2)
+      call m%init(n*n*4,node(0,[0,0]),eq,cmp,sw)
+      call m%insert(node(0,idx))
       flag=0
-      do while(m%size>0)
+      outer:do while(m%size>0)
          call m%pop(x)
-         id=flag(x%mv,x%idx(1),x%idx(2))
-         if(id/=0)cycle
-         flag(x%mv,x%idx(1),x%idx(2))=1
-         idy=x%idx+move(:,x%mv)
-         select case(a(idy(1),idy(2)))
-         case(1)
-         case(0)
-            call m%insert(node(x%lens+1,idy,x%mv))
-            call m%insert(node(x%lens+1,idy,rotate(1,x%mv)))
-            call m%insert(node(x%lens+1,idy,rotate(2,x%mv)))
-         case(2)
-            minlen=x%lens+1
-            exit
-         end select
-      end do
+         if(flag(x%idx(1),x%idx(2))/=0)cycle
+         flag(x%idx(1),x%idx(2))=1
+         do i=1,4
+            idy=x%idx+move(:,i)
+            if(flag(idy(1),idy(2))/=0)cycle
+            select case(a(idy(1),idy(2)))
+            case(1)
+            case(0)
+               call m%insert(node(x%lens+1,idy))
+            case(2)
+               minlen=x%lens+1
+               exit outer
+            end select
+         end do
+      end do outer
       call m%clean()
    end subroutine dijkstra
 
@@ -80,7 +75,6 @@ contains
       select type(b); class is (node)
       a%lens=b%lens
       a%idx =b%idx
-      a%mv=b%mv
       end select; end select 
    end subroutine eq
 
